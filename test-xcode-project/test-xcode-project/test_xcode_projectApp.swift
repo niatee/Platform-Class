a16b91c2b0484b532e6b03be07b2330095743662
niatee <niyati@w5nik.org>
2023-03-20 21:38:10 -0500

    add test-xcode-project again to the repository

---
+++ test-xcode-project/test-xcode-project/test_xcode_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_xcode_projectApp.swift
+//  test-xcode-project
+//
+//  Created by Niyati Belathur on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct test_xcode_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
